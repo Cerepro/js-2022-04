feae739677b6e00325d8a2f961cdf936dbf50aa3
edkab1 <edris.kabir@syntax-institut.de>
2022-04-29 10:37:56 +0200

    Aufgabe4

---
+++ lektion4 /js/lektion4.js
@@ -0,0 +1,50 @@
+
+/**
+ * Lektion 4: Bedingungen
+ */
+
+/**
+ * 1) 1) Erstelle eine if-Anweisung, die abfragt, ob ein bestimmter Artikel bereits gekauft wurde */
+function artikel_Gekauft() {
+    //ToDo: füge ab hier deinen Code ein
+
+}
+
+
+/**
+ * 2) Erstelle eine if-Abfrage, die in der Kosole ausgibt, dass ein Artikel hinzugefügt wurde,
+ * falls der `artikelName` NICHT leer ist.
+ */
+function artikel_Hinzufuegen() {
+    //ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * 3) Erstelle eine if-else-Anweisung, die überprüft ob die Gruppe `neueGruppe` bereits im leeren Array `gleicheGruppe`
+ * existiert und dementsprechend passende Sätze auf der Konsole ausgibt
+ */
+function gruppe_Hinzufuegen() {
+    // ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * 4) Überprüfe mit Hilfe einer switch-case-Anweisung, ob ein bestimmter Artikel in dem Array `einkaufsliste` vorkommt
+ */
+function einkaufsliste_Vorhanden() {
+    // ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * ZUSATZ: Durch folgenden Code sollst du als Zahl erhalten, an welcher Stelle im Array ein bestimmter Artikel steht
+ */
+function zusatz_indexOf() {
+    // ToDo: füge ab hier deinen Code ein
+
+}
+
+export {
+    artikel_Gekauft, artikel_Hinzufuegen, gruppe_Hinzufuegen, einkaufsliste_Vorhanden, zusatz_indexOf
+}
---
+++ lektion4 /js/uebung4.js
@@ -0,0 +1,59 @@
+/**
+ * Uebung 4: Bedingungen
+ */
+
+/**
+ * 1) Erstelle deine if-Bedingung!
+ */
+function aufgabe1() {
+  console.debug("Aufgabe 1 ==========================");
+
+  let ampel = "gruen";
+
+  //ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * 2) Erstelle deine if-Bedingung mit einem negativen Wert!
+ */
+function aufgabe2() {
+  console.debug("Aufgabe 2 ==========================");
+
+  let ampel = "rot";
+
+  //ToDo: füge ab hier deinen Code ein
+}
+
+/**
+ * 3) Bastel deine if-else Ampel!
+ */
+function aufgabe3() {
+  console.debug("Aufgabe 3 ==========================");
+
+  let ampel = "gruen";
+
+  //ToDo: füge ab hier Deinen Code ein
+}
+
+/**
+ * 4) Bastel deinen Switch Wochenkalender!
+ */
+function aufgabe4() {
+  console.debug("Aufgabe 4 ==========================");
+
+  let tag = 5;
+
+  //ToDo: füge ab hier Deinen Code ein
+}
+
+/**
+ * Hauptfunktion, in der die Aufgaben aufgerufen werden
+ */
+function main() {
+  aufgabe1();
+  aufgabe2();
+  aufgabe3();
+  aufgabe4();
+}
+main();
