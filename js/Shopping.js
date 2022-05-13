77620b2f2ed49a86f344fd600d4c1fe1a1632a31
Patrick 1983 Syntax <Patrick.Naumann2022@gmail.com>
2022-05-13 11:13:21 +0200

    Merge branch 'master' of https://github.com/syntaxinstitut/js-2022-04

---
+++ js/Shopping.js
@@ -0,0 +1,52 @@
+import Gruppe from "./src/model/Gruppe.js";
+
+class Shopping {
+  gruppenListe = []
+  aktiveGruppe = null
+  meldungenAusgeben = true
+
+  gruppeFinden(suchName, meldungenAusgeben) {
+    for (let gruppe of this.gruppenListe) {
+      if (gruppe.name == suchName) {
+        return gruppe
+      }
+    }
+    console.debug("Gruppe", suchName, "nicht gefunden")
+    return null
+  }
+
+  gruppeHinzufuegen(name) {
+    // gruppeFinden
+    let vorhandeneGruppe = this.gruppeFinden(name)
+    if (vorhandeneGruppe == null) {
+      let neueGruppe = new Gruppe(name.gruppenListe.length)
+      this.gruppenListe.push(neueGruppe)
+      this.informieren("[App]")
+      return neueGruppe
+    } else {
+      console.debug("Gruppe: ", name, " schon vorhanden")
+    }
+  }
+
+  informieren(nachricht, istWarnung = false) {
+    if (this.meldungenAusgeben) {
+      if (istWarnung) {
+        console.log(nachricht)
+      }else {
+        console.debug(nachricht)
+        // ToDo: Speichern
+      }
+    }
+  }
+}
+
+const App = new Shopping()
+let früchte = App.gruppeHinzufuegen("Früchte")
+console.debug(früchte)
+let früchte2 = App.gruppeHinzufuegen("Früchte")
+
+let früchteGruppe = App.gruppeFinden("früchte")
+console.debug(früchteGruppe)
+
+
+export default App
