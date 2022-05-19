3e69d0b5dc342acb572a8c49982d7f06fc4e3520
Patrick <Patrick.Naumann2022@Gmail.com>
2022-05-19 15:36:12 +0200

    19.05.22

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -11,8 +11,63 @@ class ShoppingList extends React.Component {
       //füge hier deinen HTML-Code ein
       <div id="container">
         {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        Füge hier Deinen HTML-Code ein<br/>
-        <GruppenTag/>
+
+        <header>
+          <h1>Einkaufsliste 2</h1>
+          <nav>
+            <input type="text" placeholder="Artikel hinzufügen"/>
+            <button className="material-icons" >add_circle</button>
+          </nav>
+        </header>
+
+        <hr/>
+
+        <main>
+          <section>
+            <h2>Einkaufen
+              <i className="material-icons">expand_less</i>
+            </h2>
+            <dl>
+              <dt>Obst & Gemüse
+                <i className="material-icons">expand_less</i>
+              </dt>
+              <dd><label><input type="checkbox"/> Brokkoli</label></dd>
+              <dt >Getreideprodukte
+                <i className="material-icons">expand_less</i>
+              </dt>
+              <dd><label><input type="checkbox"/> Reis</label></dd>
+              <dt >Milchprodukte
+                <i className="material-icons">expand_less</i>
+              </dt>
+            </dl>
+          </section>
+          <hr/>
+          <section>
+            <h2>Erledigt
+              <i className="material-icons">expand_less</i>
+            </h2>
+            <dl>
+              <dt >Hülsenfrüchte
+                <i className="material-icons">expand_less</i>
+              </dt>
+              <dd><label><input type="checkbox"/> <s>Tofu</s></label></dd>
+            </dl>
+          </section>
+          <hr/>
+        </main>
+        <footer>
+          <nav>
+            <button>
+              <span className="material-icons">bookmark_add</span> Gruppen
+            </button>
+            <button>
+              <span className="material-icons">sort</span> Sortieren
+            </button>
+            <button>
+              <span className="material-icons">settings</span> Einstellungen
+            </button>
+          </nav>
+        </footer>
       </div>
     )
   }
