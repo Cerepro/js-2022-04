ca818374f3db4435ec0279c5d14c8d042c160113
coach-alfred <alfred.walther@artingo.net>
2022-05-24 10:41:32 +0200

    ViewMode und EditMode implementiert

--- src/components/ArtikelTag.js
+++ src/components/ArtikelTag.js
@@ -3,6 +3,28 @@ import React from 'react'
 class ArtikelTag extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      // schaltet den Edit-Mode um
+      isEditing: false,
+      // enthält den neuen Namen im Edit-Mode
+      newName: this.props.artikel.name
+    }
+  }
+
+  handleChange(event) {
+    this.setState({newName: event.target.value})
+  }
+
+  /**
+   * Benennt einen Artikel um
+   * @param {Artikel} artikel - der umzubenennende Artikel
+   * @param {Event.KEYPRESS} event -
+   */
+  artikelUmbenennen(artikel, event) {
+    if (event && event.key != "Enter") return
+    // ToDo: Modell.aktuelleGruppe.artikelUmbenennen() verwenden
+    artikel.name = this.state.newName
+    this.setState({isEditing: false})
   }
 
   render() {
@@ -12,14 +34,34 @@ class ArtikelTag extends React.Component {
       artikelName = <s>{artikel.name}</s>
     }
 
+    const viewTemplate = (
+      <dd>
+        <label><input type="checkbox"/>{artikelName}</label>
+        {!artikel.gekauft ?
+          <i className="material-icons"
+             onClick={() => this.setState({isEditing: true})}>edit</i>
+          : ""
+        }
+        <i className="material-icons">delete</i></dd>
+    )
+
+    let editTemplate = (
+      <dd>
+        <input type="search" value={this.state.newName} autoFocus={true}
+               onChange={event => this.handleChange(event)}
+               onKeyPress={event => this.artikelUmbenennen(artikel, event)}/>
+        <i className="material-icons"
+           onClick={() => this.setState({isEditing: false})}>cancel </i>
+        <i className="material-icons"
+           onClick={() => this.artikelUmbenennen(artikel)}>check_circle </i>
+      </dd>
+    )
+
+
     return (
-      <React.Fragment>
-        {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        <dd><label>
-          <input type="checkbox"/>
-          {artikelName}
-        </label></dd>
-      </React.Fragment>
+      this.state.isEditing
+        ? editTemplate
+        : viewTemplate
     )
   }
 }
